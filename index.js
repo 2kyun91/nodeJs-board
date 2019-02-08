3d70614e69b45ab44ec62282280145c1be5ef0ac
2kyun91 <kim78boy@naver.com>
2019-02-08 17:58:07 +0900

    second commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var app = express();
+
+// DB 세팅
+mongoose.connect(process.env.MONGO_DB, {useNewUrlParser:true});
+var db = mongoose.connection;
+
+db.once("open", function() {
+  console.log("DB connected");
+});
+
+db.on("error", function(err) {
+  console.log("DB error : " + err);
+});
+
+// ejs 세팅
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+
+// Routes
+app.use("/", require("./routes/home"));
+
+// port 세팅
+app.listen(3000, function() {
+  console.log("server on!");
+});
